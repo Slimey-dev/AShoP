13d02bb6d6675b45ff344e236aaf05b1bf637b4a
Slimey-dev <75830341+Slimey-dev@users.noreply.github.com>
2023-04-09 11:48:12 +0200

    init
    Took 47 seconds

---
+++ AShoP/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace AShoP.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public ICollection<Item>? Items { get; set; }
+    public ICollection<Category>? SubCategories { get; set; }
+}
---
+++ AShoP/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace AShoP.Models;
+
+public class Item
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
+    public Category? Category { get; set; }
+    public List<string>? Tags { get; set; }
+}
